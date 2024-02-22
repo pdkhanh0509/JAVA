b3eade4bea23bcfdf45454d957a1bbfabdbaf1e4
pdkhanh0509@gmail.com <pdkhanh0509@gmail.com>
2024-02-22 17:02:32 +0700

    Initial commit

---
+++ Java_04/src/ViDu.java
@@ -0,0 +1,12 @@
+public class ViDu {
+    public static void main(String[] args) {
+        String thongBao = "Hello";
+        String hoVaTen = "Pham Duy Khanh";
+        String tenSach;
+        tenSach = "Lap trinh Java";
+
+        System.out.println("Thong Bao: "+ thongBao);
+        System.out.println("Ho & Ten: "+ hoVaTen);
+        System.out.println("Ten Sach: "+ tenSach);
+    }
+}
